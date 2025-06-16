4a282e3737948d597d9b1471139e0935d27ce8a9
Darius Neațu <neatudarius@gmail.com>
2025-06-16 13:43:13 +0300

    beman-tidy: ruff linter tweaks (#1)

--- tools/beman-tidy/lib/checks/beman_standard/toplevel.py
+++ tools/beman-tidy/lib/checks/beman_standard/toplevel.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from ..base.file_base_check import FileBaseCheck
-
 # TODO TOPLEVEL.CMAKE
 # TODO TOPLEVEL.LICENSE
 # TODO TOPLEVEL.README
